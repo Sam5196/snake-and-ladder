1918df3d299624149d9438b24f9f8fbd0e9f432d
Sam5196 <sammedpachore1008@gmail.com>
2022-07-07 17:51:01 +0530

    Added: Current position is start

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder {
+    public static final int START_POSITION = 0;
+    public static final int FINAL_POSITION = 100;
+    public static int currentPosition;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Game!!");
+        currentPosition = START_POSITION;
+    }
+}
